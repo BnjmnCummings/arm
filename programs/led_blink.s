92e8f3c3e96ec45cc8f488553d6e38e943250336
Eli Gilbert <eg1923@ic.ac.uk>
2024-06-18 19:40:53 +0100

    wrote some code to complete part III, also setup a second file with comments on the code

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,8 +1,27 @@
-TODO(Implementation of part III goes here)
-Should follow:
-    select an arbitrary pin p
-    continuously:
-        set p to output a high signal (GPSET)
-        wait (branches)
-        set p to low signal (GPCLR)
-        wait (branches)
+x0 = 0x3f20000
+ldr w1, #64
+str w1, [x0]
+x2 = 0x3f20001c
+ldr w3, #4
+x4 = 0x3f200028
+b on
+
+waitAfterOn:
+sub w10, w10, #1
+b.ne waitAfterOn
+b off
+
+on:
+str w3, [x2]
+movk w10, #50, lsl #16
+b waitAfterOn
+
+waitAfterOff:
+sub w10, w10, #1
+b.ne waitAfterOff
+b on
+
+off:
+str w3, [x4]
+movk w10, #50, lsl #16
+b waitAfterOff
