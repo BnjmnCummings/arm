c2b96802f2c7a0a921c31bd4c6e1f1b956c76a92
Eli Gilbert <eg1923@ic.ac.uk>
2024-06-19 17:52:26 +0100

    Debugged the bugs and it works :)! Fixes:     1) replaced sub with subs     2) replaced 0x3f2 with 0x3f20

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,29 +1,29 @@
-movz x0, #0x3f2, lsl #16
+movz x0, #0x3f20, lsl #16
 movz w1, #64
 str w1, [x0]
 
 add x2, x0, #0x28
 movz w3, #4
 movk x0, #0x1c
-movz w5 #50, lsl #16
+movz w5, #50, lsl #16
 b on
 
 waitAfterOn:
-sub w10, w10, #1
-b.ne waitAfterOn
-b off
+    subs w10, w10, #1
+    b.ne waitAfterOn
+    b off
 
 on:
-str w3, [x0]
-movz w10, w5
-b waitAfterOn
+    str w3, [x0]
+    mov w10, w5
+    b waitAfterOn
 
 waitAfterOff:
-sub w10, w10, #1
-b.ne waitAfterOff
-b on
+    subs w10, w10, #1
+    b.ne waitAfterOff
+    b on
 
 off:
-str w3, [x2]
-movz w10, w5
-b waitAfterOff
+    str w3, [x2]
+    mov w10, w5
+    b waitAfterOff
